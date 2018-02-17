2851efe6d2a6abdd175f52d31e5111b17a51e032
Jay Hayes <ur@iamvery.com>
2018-02-17 12:21:37 -0600

    Add pact with animals client library

---
+++ producer/spec/service_consumers/pact_helper.rb
@@ -0,0 +1,21 @@
+require 'pact/provider/rspec'
+
+Pact.provider_states_for 'Animals Client' do
+  provider_state 'some cats exist' do
+    set_up do
+      Cat.create!(name: 'Garfield')
+      Cat.create!(name: 'Felix')
+      Cat.create!(name: 'Silvester')
+    end
+
+    tear_down do
+      Cat.destroy_all
+    end
+  end
+end
+
+Pact.service_provider 'Animals Service' do
+  honours_pact_with 'Animals Client' do
+    pact_uri '../producer_client/spec/pacts/animals_client-animals_service.json'
+  end
+end
