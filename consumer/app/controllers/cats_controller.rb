211bc820f738e5ac3498e3fe25ebd2c7151570ea
Jay Hayes <ur@iamvery.com>
2018-02-24 09:15:53 -0600

    Implement consumer cat creation through service lib

--- consumer/app/controllers/cats_controller.rb
+++ consumer/app/controllers/cats_controller.rb
@@ -2,4 +2,15 @@ class CatsController < ApplicationController
   def index
     @cats = ANIMALS.cats.list
   end
+
+  def create
+    ANIMALS.cats.create(cat_params)
+    redirect_to cats_path
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :age)
+  end
 end
--- consumer/spec/features/cats_spec.rb
+++ consumer/spec/features/cats_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature 'Cats', type: :feature do
-  it 'lists all known cats' do
+  it 'creates and lists all known cats' do
     visit cats_path
+
+    add_cat(name: 'Garfield', age: 39)
+    add_cat(name: 'Felix', age: 99)
+    add_cat(name: 'Sylvester', age: 76)
 
     expect(page).to have_content('Garfield (39)')
     expect(page).to have_content('Felix (99)')
     expect(page).to have_content('Sylvester (76)')
+  end
+
+  def add_cat(name:, age:)
+    click_on 'Add'
+    fill_in 'Name', with: name
+    fill_in 'Age', with: age
+    click_on 'Save Cat'
   end
 end
