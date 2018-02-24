fb00a501c57e7899fa9e1abf289ed8c91d747fbe
Jay Hayes <ur@iamvery.com>
2018-02-23 18:26:11 -0600

    Flatten cats feature in tour style

--- consumer/spec/features/cats_spec.rb
+++ consumer/spec/features/cats_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'Cats', type: :feature do
-  describe 'index' do
-    it 'lists all known cats' do
-      visit cats_path
+  it 'lists all known cats' do
+    visit cats_path
 
-      expect(page).to have_content('Garfield (39)')
-      expect(page).to have_content('Felix (99)')
-      expect(page).to have_content('Sylvester (76)')
-    end
+    expect(page).to have_content('Garfield (39)')
+    expect(page).to have_content('Felix (99)')
+    expect(page).to have_content('Sylvester (76)')
   end
 end
